41868d23a9e2c5e25f6a0384d2e9da9f0d9be115
MatiasE <mepi-11@hotmail.com>
2021-02-23 17:03:45 -0300

    Limpiando Proyecto Poniendo el Proyecto en orden para empezar a aprender React!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello Matias in CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  return <div>Hello World</div>;
 }
