fa5dd5108cd5d6b8c01b0c1ec66fcba482aafa8d
MatiasE <mepi-11@hotmail.com>
2021-02-24 11:14:19 -0300

    Crear Componente x3 Como Crear un componente de 3 maneras distintas!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
 import "./styles.css";
+import "./App.css";
+import React from "react";
 
-export default function App() {
-  return <div>Hello World</div>;
+// 1ra Manera:
+// const App = () => (
+//  <div>
+//    This is my Component: <HelloWorld />
+//  </div>
+// );
+// export default App;
+
+// 2da Manera:
+//export default function App() {
+//  return (
+//    <div>
+//      This is my component: <HelloWorld />
+//    </div>
+//  );
+// }
+
+// 3ra Manera:
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        This is my Component: <HelloWorld />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+export function HelloWorld() {
+  return <div id="hello">Hello World</div>;
 }
